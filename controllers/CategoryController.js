a679ddc806ad9f7265a1cbee13950783623890e8
anggiirawan13 <irawananggi76@gmail.com>
2023-03-25 03:25:28 +0700

    init

---
+++ controllers/CategoryController.js
@@ -0,0 +1,62 @@
+import category from '../models/Category.js'
+
+const index = async (req, res) => {
+    try {
+        const categories = await category.find({status: 'active'});
+
+        if (!categories) {
+            throw {
+                code: 500,
+                message: "Get categories failed!",
+            }
+        }
+
+        return res.status(200).json({
+            status: true,
+            total: categories.length,
+            categories,
+        })
+    } catch (err) {
+        return res.status(err.code).json({
+            status: false,
+            message: err.message,
+        })
+    }
+}
+
+const store = async (req, res) => {
+    try {
+        if (!req.body.title) {
+            throw {
+                code: 428,
+                message: "Title is required!",
+            }
+        }
+        const title = req.body.title;
+
+        const newCategory = new category({
+            title: title,
+        })
+
+        const Category = await newCategory.save();
+
+        if(!Category) {
+            throw {
+                code: 500,
+                message: "Store category failed",
+            }
+        }
+
+        return res.status(200).json({
+            status: true,
+            Category,
+        })
+    } catch (err) {
+        return res.status(err.code).json({
+            status: false,
+            message: err.message,
+        })
+    }
+}
+
+export {index, store};
